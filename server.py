6f10c57af52eb3915ad259185cebee4fe672243a
Mbuso  Mgobhozi <mbuso.mgobhozi@rain.co.za>
2024-10-24 14:23:26 +0200

    Track Progress

--- server.py
+++ server.py
@@ -7,14 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/emotionDetector", methods=["Post"])
+@app.route("/emotionDetector", methods=["Get"])
 def emotion_detection():
     data = request.get_json()
-    text_to_analyze = data.get('text', 'I love my life')
+    text_to_analyze = data.get('text', '')
 
     result = emotion_detector(text_to_analyze)
+    dominant_emotion = result.get("dominant_emotion")
+    result.pop("dominant_emotion")
 
-    return jsonify({'emotion': result})
+    return f"For the given statement, the system response is {result}. The dominant emotion is {dominant_emotion}"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
