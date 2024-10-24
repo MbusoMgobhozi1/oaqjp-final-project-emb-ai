418662e8d5944bee5191fa130a5b63a4184721f7
MbusoMgobhozi1 <mgobhozi360@icloud.com>
2024-10-24 04:07:59 -0400

    Track Progress Final Project

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    json_data = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url=url, headers=headers, json=json_data) 
+    json_results = json.loads(response.text)
+    response_dict = json_results.get("emotionPredictions")[0].get("emotion")
+
+    dominant_emotion = None
+    dominant_value = float(0)
+
+    for emotion, value in response_dict.values():
+        if value > dominant_value:
+            dominant_value = value
+            dominant_emotion = emotion
+        
+    response_dict["dominant_emotion"] = dominant_emotion
+
+    return response_dict
