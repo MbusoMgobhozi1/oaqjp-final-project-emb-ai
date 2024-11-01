67ec652d7aa2e5ae3b83754d615e8a17e557fa88
Mbuso  Mgobhozi <mbuso.mgobhozi@rain.co.za>
2024-11-01 08:39:05 +0200

    Implement Logging And Fix We Fetch The Text

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, render_template, jsonify
 from EmotionDetection import emotion_detector
+import logging
 
+log = logging.getLogger()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route("/emotionDetector", methods=["Get"])
+@app.route("/emotionDetector", methods=["GET", "POST"])
 def emotion_detection():
-    data = request.get_json()
-    text_to_analyze = data.get('text', '')
+    text_to_analyze = request.values.get('textToAnalyze', '')
+    log.info("Received text to analyze: %s", text_to_analyze)
 
     result = emotion_detector(text_to_analyze)
     dominant_emotion = result.get("dominant_emotion")
@@ -19,4 +21,4 @@ def emotion_detection():
     return f"For the given statement, the system response is {result}. The dominant emotion is {dominant_emotion}"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(port=5000, debug=True)
