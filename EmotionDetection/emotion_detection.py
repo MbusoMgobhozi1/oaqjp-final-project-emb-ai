0dfe4cbc5608fc98079871487a7d6f3ef0379bd1
Mbuso Mgobhozi <110095513+MbusoMgobhozi1@users.noreply.github.com>
2024-11-01 10:20:01 +0200

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,7 +5,7 @@ import logging
 log = logging.getLogger(__name__)
 
 def emotion_detector(text_to_analyse):
-    url = 'https://sn-watson-emotion.labs.skills.network/v2/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_data = { "raw_document": { "text": text_to_analyse } }
 
